b37a7ecd191626d5adebb64b2eb15863d627c51e
sammesenekal <ssenekal@student.ubc.ca>
2026-02-06 10:58:45 -0800

    Processed Theoph dataset from R, wrote CSV files, and produced processed data showing only the three variables that I want to look at for this project: Subject, Time, Dose.

---
+++ scripts/data_variable-processing.R
@@ -0,0 +1,13 @@
+library(here)
+library(tidyverse)
+library(readr)
+
+readr::write_csv(Theoph, here::here("data", "data_raw", "Theoph.csv"))
+
+my_Theoph <- readr::read_csv(here::here("data","data_raw","Theoph.csv"))
+
+my_Theoph_threevar <- my_Theoph |>
+  select(Subject, Dose, Time)
+
+readr::write_csv(my_Theoph_threevar, here::here("data", "data_processed", "Theoph_threevar.csv"))
+                 
