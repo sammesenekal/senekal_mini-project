1dddf0a1f1e3059ebc6b0ed708fa372a7d3b4a06
sammesenekal <ssenekal@student.ubc.ca>
2026-02-09 10:10:26 -0800

    Selected for the wrong variable initially. Dose switched for concentration.

--- scripts/data_variable-processing.R
+++ scripts/data_variable-processing.R
@@ -7,7 +7,7 @@ readr::write_csv(Theoph, here::here("data", "data_raw", "Theoph.csv"))
 my_Theoph <- readr::read_csv(here::here("data","data_raw","Theoph.csv"))
 
 my_Theoph_threevar <- my_Theoph |>
-  select(Subject, Dose, Time)
+  select(Subject, conc, Time)
 
 readr::write_csv(my_Theoph_threevar, here::here("data", "data_processed", "Theoph_threevar.csv"))
                  
